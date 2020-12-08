459ac49cc443aba6a48729feedd74ab30f805c0a
Jonah Walker <supposedlysam@gmail.com>
2020-12-07 21:40:31 -0500

    Remove boilerplate from landing pages

--- lib/landing_3.dart
+++ lib/landing_3.dart
@@ -1,6 +1,5 @@
 import 'package:demoapp/components/fractional_rubric_card.dart';
 import 'package:demoapp/components/rubric_card.dart';
-import 'package:demoapp/components/navbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -8,47 +7,30 @@ class Landing3 extends StatelessWidget {
   static const spacing = 30.0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      resizeToAvoidBottomPadding: false,
-      extendBodyBehindAppBar: true,
-      extendBody: true,
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        backgroundColor: Color(0xFFFFAD00),
-        foregroundColor: Color(0xFF6E27BC),
-        onPressed: () {},
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      body: SafeArea(
-        bottom: false,
-        child: ListView(
-          children: [
-            logo(),
-            SizedBox(height: 30),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 12),
-              child: RubricCard(height: 125),
-            ),
-            SizedBox(height: 30),
-            scrollAreaOne(),
-            SizedBox(height: 30),
-            scrollAreaTwo(),
-            SizedBox(height: 30),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 12),
-              child: Row(
-                children: <Widget>[
-                  Expanded(child: RubricCard(height: 260)),
-                  SizedBox(width: spacing),
-                  Expanded(child: RubricCard(height: 260)),
-                ],
-              ),
-            ),
-          ],
+    return ListView(
+      children: [
+        logo(),
+        SizedBox(height: 30),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: RubricCard(height: 125),
         ),
-      ),
-      bottomNavigationBar: NavBar(),
+        SizedBox(height: 30),
+        scrollAreaOne(),
+        SizedBox(height: 30),
+        scrollAreaTwo(),
+        SizedBox(height: 30),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: Row(
+            children: <Widget>[
+              Expanded(child: RubricCard(height: 260)),
+              SizedBox(width: spacing),
+              Expanded(child: RubricCard(height: 260)),
+            ],
+          ),
+        ),
+      ],
     );
   }
 
